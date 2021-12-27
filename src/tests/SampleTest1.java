e4508113085ec35c230e72d5caed84b8189ae294
Johnson Vu <6262574+johnsonvu911@users.noreply.github.com>
2021-12-27 17:00:38 +0700

    Update SampleTest1.java

--- src/tests/SampleTest1.java
+++ src/tests/SampleTest1.java
@@ -23,21 +23,30 @@ public class SampleTest1 extends TestBase {
 
         seleniumHelper.navigateTo(Constants.testSite);
         loginPage.login(Constants.loginEmail, Constants.loginPassword);
-
+        // Navigate to Inventory page 
         homePage.clickMenuItemButton(Constants.MenuItem.get("inventory"));
+        // Navigate to Products page via menu bar 
         commonActions.navigateToProductsPage();
+        // Create a new product 
         inventoryPage.createProduct(productName);
         commonActions.waitForPageDisplayed("Update Quantity");
+        // Update the product's quantity 
         inventoryPage.updateQuantity(Constants.productQuantity);
-
+        
+        // Go back to home page 
         commonActions.clickMenuToggle();
 
+        // Navigate to Manufacturing page 
         homePage.clickMenuItemButton(Constants.MenuItem.get("manufacturing"));
+        // Create a manufacturing order 
         manufacturingPage.createOrder(productName, "1");
+        // Get manufacturing order name and product name for verification later 
         String orderNameBefore = manufacturingPage.getOrderName();
         String productNameBefore = manufacturingPage.getProductName();
+        // Update the manufacturing order to Done from draft. 
         manufacturingPage.updateNewOrderToDone(orderNameBefore);
 
+        // CHECKPOINT: Verify the newly-created manufacturing order with order name and product name. 
         Assert.assertTrue(manufacturingPage.validateOrder(orderNameBefore, productNameBefore));
 
     }
